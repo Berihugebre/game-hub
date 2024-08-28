b1b2b6a5ddc73f0a61bf941287ec6f6ad882158d
berihugebre <berihugebremedhin@Berihus-MacBook-Pro.local>
2024-08-28 12:50:17 +0200

    handling games without images

--- src/service/image-url.ts
+++ src/service/image-url.ts
@@ -1,4 +1,7 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImageUrl = (url: string) =>{
+    if(!url) return noImage;
     const splitted = url.split('/media/');
     const cropped = `${splitted[0]}/media/crop/600/400/${splitted[1]}`;
     return cropped;
